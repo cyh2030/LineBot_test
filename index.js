91034b39e09f8d40bb4a0dc5d4bb8ee229c0436b
cyh2030 <51306323+cyh2030@users.noreply.github.com>
2023-01-03 13:09:26 +0800

    finished message handling 0103-3

--- index.js
+++ index.js
@@ -97,9 +97,8 @@ app.listen(process.env.PORT || 3000, () => {
 
 //定時推播訊息
 const config = {
-  channelAccessToken:
-    "bW3urfAiN8Lg0BgF175qpzBGbvlT2SVAN9dekAtXSHotmFwdxFPE0DWURxFDRXYVXPaso1sMDxJbFy6CVgMT6vXglbaXVSZWCZcNiZWeQ+Md413jHxpo0l16xbiYEN0WA0r2NWw8vtyn552lsaatZwdB04t89/1O/w1cDnyilFU=",
-  channelSecret: "48d69d737406be7f8f478861f9be295d",
+  channelAccessToken: process.env.channelAccessToken,
+  channelSecret: process.env.channelSecret,
 };
 
 const client = new line.Client(config);
